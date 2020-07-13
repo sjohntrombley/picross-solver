a320c8660779a2f6088bb47c48215c3f9ea09aaf
S. John Trombley <s.john.trombley@gmail.com>
2020-07-12 20:02:32 -0600

    nice and clean

--- picross.py
+++ picross.py
@@ -167,15 +167,12 @@ def merge_gaps(clues, updates, gap):
                     updates[cell_index]['state'] = Cell.unknown
         offset += gap[clue_index+1]
 
-def solve2(p):
+def solve(p):
     if type(p) is not Puzzle:
-        raise TypeError("First argument to solve2 must be of Puzzle type.")
+        raise TypeError("First argument to solve must be of Puzzle type.")
     grid = Grid(len(p.row_clues), len(p.column_clues))
     row_clues, col_clues = p.row_clues, p.column_clues
     update, done = True, False
-    #pass_n = 1
-    #print(grid)
-    #input('Do row pass?')
     while update and not done:
         update, done = False, True
         # Row pass
@@ -207,22 +204,16 @@ def solve2(p):
         if done:
             break
         done = True
-        #print(f'pass: {pass_n}')
-        #print(grid)
-        #input('Do col pass?')
 
         # Column pass
         for col_index,this_col_clues in enumerate(col_clues):
-            #print(f'==> col_index: {col_index}')
-            #print(f'==> this_col_clues: {this_col_clues}')
             this_col = [ row[col_index] for row in grid ]
-            #print(f'==> this_col: {pretty_slice(this_col)}')
 
             # Go to next col if this col is done
             if all(state is not Cell.unknown for state in this_col):
                 continue
 
-            # Check for empty rows
+            # Check for empty cols
             if len(this_col_clues)==0:
                 for row_index in range(len(this_col)):
                     grid[row_index][col_index] = Cell.empty
@@ -230,14 +221,11 @@ def solve2(p):
                 continue
 
             this_col_updates = [ {'state':state, 'locked':state is not Cell.unknown} for state in this_col ]
-            #print(f'==> [before]this_col_updates: {pretty_update(this_col_updates)}')
             # Loop through possible arrangements
             for gap in gaps_gen(this_col_clues, len(this_col)):
                 merge_gaps(this_col_clues, this_col_updates, gap)
-            #print(f'==> [after]this_col_updates: {pretty_update(this_col_updates)}')
 
             # Update the grid
-            #print(f'{this_col}\n{this_col_updates}')
             for row_index, cell in enumerate(this_col_updates):
                 if cell['locked'] and cell['state'] is Cell.unknown:
                     done = False
@@ -245,18 +233,7 @@ def solve2(p):
                     #print(f'{row_index} {col_index}')
                     grid[row_index][col_index] = cell['state']
                     update = True
-        #print(grid)
-        #input('Do row pass?')
     return grid
-
-#def main():
-#    row_clues = ''
-#    while row_clues == '':
-#        row_clues = input('Enter row clues: ').strip()
-#        if row_clues[0]!='[' or row_clues[-1]!=']':
-#            row_clues = ''
-#            continue
-#        row_clues
 
 def main():
     n_rows, n_cols = 0, 0
@@ -269,112 +246,51 @@ def main():
                 n_cols = int(dimensions[1])
             except ValueError:
                 n_row, n_cols = 0, 0
-                print('Error: Invalid dimensions. Dimensions should be input in the form rxc where are is the number of rows and c is the number of columns')
+                print(
+                        'Error: Invalid dimensions. Dimensions should be ' +
+                        'input in the form rxc where are is the number of ' +
+                        'rows and c is the number of columns.'
+                        )
         else:
-            print('Error: Invalid dimensions. Dimensions should be input in the form rxc where are is the number of rows and c is the number of columns')
+            print(
+                    'Error: Invalid dimensions. Dimensions should be input ' +
+                    'in the form rxc where are is the number of rows and c ' +
+                    'is the number of columns.'
+                    )
 
     row_clues = []
     for row_index in range(n_rows):
         valid_clue = False
         while not valid_clue:
             try:
-                row_clues.append([int(clue) for clue in input(f'Enter clues for row {row_index+1}: ').split(' ')])
+                row_clues.append([
+                    int(clue) for clue in
+                    input(f'Enter clues for row {row_index+1}: ').split(' ')
+                    ])
                 valid_clue = True
             except ValueError:
-                print('Error: Invalid clue. Clues should be a list of positive integers separated by spaces.')
+                print(
+                        'Error: Invalid clue. Clues should be a list of ' +
+                        'positive integers separated by spaces.'
+                        )
 
     col_clues = []
     for col_index in range(n_cols):
         valid_clue = False
         while not valid_clue:
             try:
-                col_clues.append([int(clue) for clue in input(f'Enter clues for col {col_index+1}: ').split(' ')])
+                col_clues.append([
+                    int(clue) for clue in
+                    input(f'Enter clues for col {col_index+1}: ').split(' ')
+                    ])
                 valid_clue = True
             except ValueError:
-                print('Error: Invalid clue. Clues should be a list of positive integers separated by spaces.')
+                print(
+                        'Error: Invalid clue. Clues should be a list of ' +
+                        'positive integers separated by spaces.'
+                        )
     p = Puzzle(row_clues, col_clues)
-    print(solve2(p))
+    print(solve(p))
 
 if __name__ == "__main__":
     main()
-
-#def solve(p):
-#    if type(p) is not Puzzle:
-#        raise TypeError(type(p))
-#    grid = Grid(p.row_clues, p.column_clues)
-#    row_clues, col_clues = p.row_clues, p.column_clues
-#    update, done = True, False
-#    while update and not done:
-#        update, done = False, True
-#        for i in range(len(row_clues)):
-#            row = grid[i].copy()
-#            if all(cell is not Cell.unknown for cell in row):
-#                continue
-#            if len(row_clues[i]) == 0:
-#                grid[i] = [ Cell.empty for _ in row ]
-#                update = True
-#                continue
-#            for j in range(len(row_clues[i])):
-#                clue_min = 0
-#                for clue in row_clues[i][:j]:
-#                    while (
-#                        clue_min+clue+row_clues[i][j] < len(row)
-#                        and (
-#                            any(cell is Cell.empty for cell in row[clue_min:clue_min+clue])
-#                            or row[clue_min+clue] is Cell.filled
-#                            )
-#                        ):
-#                        clue_min += 1
-#                    if clue_min+clue+row_clues[i][j] == len(row):
-#                        raise f'Impossible! row: {i} clue: {j} grid:\n{grid}'
-#                    clue_min += clue+1
-#                clue_max = len(row) - 1
-#                for clue in reversed(row_clues[i][j+1:]):
-#                    while (
-#                        clue_min+row_clues[i][j]+clue <= clue_max
-#                        and (
-#                            any(cell is Cell.empty for cell in row[clue_max-clue+1:clue_max+1])
-#                            or row[clue_max-clue] is Cell.filled
-#                            )
-#                        ):
-#                        clue_max -= 1
-#                    if clue_min+row_clues[i][j]+clue > clue_max:
-#                        raise f'Impossible! row: {i} clue: {j} grid:\n{grid}'
-#                    clue_max -= clue+1
-#                while clue_min+row_clues[i][j] <= clue_max+1 and row[clue_min] is Cell.empty:
-#                    clue_min += 1
-#                if clue_min+row_clues[i][j] > clue_max+1:
-#                    raise f'Impossible! row: {i} clue: {j} grid:\n{grid}'
-#                while clue_min+row_clues[i][j] <= clue_max+1 and row[clue_max] is Cell.empty:
-#                    clue_max -= 1
-#                if clue_min+row_clues[i][j] > clue_max+1:
-#                    raise f'Impossible! row: {i} clue: {j} grid:\n{grid}'
-#                if all(cell is not Cell.empty for cell in row[clue_min:clue_max+1]):
-#                    if clue_max-clue_min+1 < 2*row_clues[i][j]:
-#                        blank = clue_max-clue_min+1-row_clues[i][j]
-#                        grid[i][clue_min+blank:clue_min+row_clues[i][j]] = [Cell.filled] * (row_clues[i][j]-blank)
-#                        update = True
-#                        row = grid[i].copy()
-#                    continue
-#                gaps = [0]
-#                for cell in row[clue_min:clue_max+1]:
-#                    if cell is cell.empty:
-#                        if gaps[-1] != 0:
-#                            gaps.append(0)
-#                        continue
-#                    gaps[-1] += 1
-#                if all(gap < row_clues[i][j] for gap in gaps):
-#                    raise f'Impossible! row: {i} clue: {j} grid:\n{grid}'
-#                if len([gap for gap in gaps if gap >= row_clues[i][j]]) > 1:
-#                    continue
-#                while any(cell is Cell.empty for cell in row[clue_min:clue_min+row_clues[i][j]]):
-#                    while row[clue_min] is not Cell.empty:
-#                        clue_min += 1
-#                    while row[clue_min] is Cell.empty:
-#                        clue_min += 1
-#                gap = [gap for gap in gaps if gap >= row_clues[i][j]][0]
-#                if gap < 2*row_clues[i][j]:
-#                    blank = gap - row_clues[i][j]
-#                    grid[i][clue_min+blank:clue_min+row_clues[i][j]] = [Cell.filled] * (row_clues[i][j]-blank)
-#                    update = True
-#            # Check for new blanks and connections
